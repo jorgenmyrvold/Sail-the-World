4c3dbcc1dd758c7108077a6557f5965fbe3da8a0
Ole Nesheim <olenesheim@oles-mbp.lan>
2020-09-17 22:07:13 +0200

    Endret på nytt I/O på ultrasonic

--- sensor/ultrasonic/ultrasonic.py
+++ sensor/ultrasonic/ultrasonic.py
@@ -1,7 +1,7 @@
 from gpiozero import DistanceSensor
 from time import sleep
 
-sensor = DistanceSensor(echo=18, trigger=4)
+sensor = DistanceSensor(echo=12, trigger=7)
 while True:
     print('Distance: ', sensor.distance * 100)
     sleep(1)

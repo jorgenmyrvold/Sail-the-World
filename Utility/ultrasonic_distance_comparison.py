11c3f0b5900d61e5d2257be8ea80ab00663ca230
Elmeister <eldar.sandanger@gmail.com>
2020-09-18 09:02:29 +0200

    Last update before merge

--- Utility/ultrasonic_distance_comparison.py
+++ Utility/ultrasonic_distance_comparison.py
@@ -3,4 +3,3 @@ In this script we have tool for comparing differences in distances to the wall f
 This can be useful for when driving along the wall keeping straight and parallell all the way along.
  """
 
-

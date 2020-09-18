0d588fee7d678d30fa41e6b41607c9acaaeaebcb
Elmeister <eldar.sandanger@gmail.com>
2020-09-18 11:02:22 +0200

    Encoder initial progress

--- motor/encoder.py
+++ motor/encoder.py
@@ -4,11 +4,21 @@ This file is just a class definiton for the encoder for practical usage.
 
 class Encoder:
 
-    _number_of_holes = 10 #SETT FAKTISK VERDI
+    _number_of_holes = 20 #SJEKK AT DETTE STEMMER
+
+    '''
+    To get a reading status from the sensor, we can use that 
+     * Hole -> true
+     * Not hole -> false
+    '''
+    
 
     def __init__(self, pin_in, pin_out, orientation)
         self.pin_in = pin_in
         self.pin_out = pin_out
+
+        #Set initial position
+        self.position = self.readValue()
 
         if(orientation_of_motor == "Left"):
             self.orientation = "Left"
@@ -16,6 +26,8 @@ class Encoder:
             self.orientation = "Right"
         else:
             raise Exception("The orientation of the motor must be 'Left' or 'Right'!")
+
+    def readValue(self):
 
     
 

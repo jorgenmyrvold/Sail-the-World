eb7f651705e9c4e3b24170147c46d4ff2f0c4c63
ludvikka <ludkasbo@gmail.com>
2020-10-05 14:38:59 +0200

    Fikset noen feil i koden

--- motor/encoder.py
+++ motor/encoder.py
@@ -4,7 +4,7 @@ This file is just a class definiton for the encoder for practical usage.
 Encoderleft_pin = 23
 
 import RPi.GPIO as GPIO
-
+import time
 import math
 import signal   #muligens overflødig  ----Vurdert slettet              
 import sys

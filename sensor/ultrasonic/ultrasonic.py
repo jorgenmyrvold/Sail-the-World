40c4c65c4f0e4055db39c090da234f86fc57fca6
Ole Nesheim <olenesheim@oles-mbp.lan>
2020-10-07 11:06:34 +0200

    Changed GPIO-pin of ultrasonic sensor

--- sensor/ultrasonic/ultrasonic.py
+++ sensor/ultrasonic/ultrasonic.py
@@ -1,7 +1,7 @@
 from gpiozero import DistanceSensor
 from time import sleep
 
-sensor = DistanceSensor(echo=18, trigger=4)
+sensor = DistanceSensor(echo=18, trigger=1)
 while True:
     print('Distance: ', sensor.distance)
     sleep(1)

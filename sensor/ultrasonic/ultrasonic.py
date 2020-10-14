f83f1753c992570c1766855be7aad437424971d8
Ole Nesheim <olenesheim@dhcp-10-220-213-91.wlan.ntnu.no>
2020-10-14 10:15:32 +0200

    Added translate-class for ultrasonic sensor

--- sensor/ultrasonic/ultrasonic.py
+++ sensor/ultrasonic/ultrasonic.py
@@ -1,7 +1,22 @@
 from gpiozero import DistanceSensor
 from time import sleep
+import numpy as np
 
-sensor = DistanceSensor(echo=18, trigger=1)
-while True:
-    print('Distance: ', sensor.distance)
-    sleep(1)
+class Ultrasonic:
+
+    def __init__(self, e, t):
+        #Translates from GPIO.BOARD to GPIO.BCM
+        board_to_bcm = np.array([-1, -1, -1, 2, -1, 3, -1, 4, 14, -1, 15, 17, 18, 27, -1, 22, 23, -1, 24, 10, -1, 9, 25, 11, 8, -1, 7, 0, 1, 5, -1, 6, 12, 13, -1, 19, 16, 26, 20, -1, 21])
+        
+        #Creating a DistanceSensor object
+        self.sensor = DistanceSensor(board_to_bcm[e], board_to_bcm[t])
+    
+    def get_distance(self):
+        return self.sensor.distance
+
+if __name__ == "__main__":
+    #sensor = DistanceSensor(echo=18, trigger=1)
+    sensor1 = Ultrasonic(36, 28)
+    while True:
+        print('Distance: ', sensor1.get_distance)
+        sleep(1)

df39ab5dd2f612963d50eca1cd4c3e094382634e
Elmeister <eldar.sandanger@gmail.com>
2020-10-21 09:22:12 +0200

    Motor test

---
+++ motor/DCmotor_test.py
@@ -0,0 +1,72 @@
+from DCMotor import *
+import RPi.GPIO as GPIO
+from time import sleep
+
+def test():
+
+    #DECIDE PINS
+    left_pwm = 13
+    left_forward = 4
+    left_backward = 17
+    right_forward = 27
+    right_backward = 19 
+    right_pwm = 12
+
+    LeftMotor = DCMotor(left_pwm, left_forward, left_backward, 'Left')
+    RightMotor = DCMotor(right_pwm, right_forward, right_backward, 'Right')
+
+    testDirections(LeftMotor)
+    testDirections(RightMotor)
+    testRange(LeftMotor)
+    testRange(RightMotor)
+
+def testDirections(motorClass):
+    
+    motorClass.turn_forward(50)
+    sleep(3)
+    motorClass.stop()
+    sleep(1)
+
+    motorClass.turn_backward(50)
+    sleep(3)
+    motorClass.stop()
+    sleep(1)
+
+    motorClass.turn_forward(100)
+    sleep(3)
+    motorClass.stop()
+    sleep(1)
+
+    motorClass.turn_backward(100)
+    sleep(3)
+    motorClass.stop()
+    sleep(1)
+
+    motorClass.shut_down() 
+
+def testRange(motorClass):
+
+    motorClass.turn_forward(10)
+    sleep(1)
+    motorClass.turn_forward(20)
+    sleep(1)
+    motorClass.turn_forward(30)
+    sleep(1)
+    motorClass.turn_forward(40)
+    sleep(1)
+    motorClass.turn_forward(50)
+    sleep(1)
+    motorClass.turn_forward(60)
+    sleep(1)
+    motorClass.turn_forward(70)
+    sleep(1)
+    motorClass.turn_forward(80)
+    sleep(1)
+    motorClass.turn_forward(90)
+    sleep(1)
+    motorClass.turn_forward(100)
+    sleep(1)
+    motorClass.stop()
+    motorClass.shut_down()
+
+test()
--- motor/servo_motor.py
+++ motor/servo_motor.py
@@ -19,6 +19,7 @@ class ServoMotor:
     # When you change the speed of a motor. Should the power be increased slowly? Too get a smoth acceleration?
     # Maybe this should be done in drive file. idk --  Ludvik
     def __init__(self, pin_out, orientation_of_motor, stop_percentage = 7.5):
+        
         self.pin_out = pin_out
         
         self.stop_percentage = stop_percentage

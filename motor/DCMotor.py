2e188eafb247ede3465aaf35211ae18cb04edb8e
Elmeister <eldar.sandanger@gmail.com>
2020-10-21 10:09:54 +0200

    check

--- motor/DCMotor.py
+++ motor/DCMotor.py
@@ -90,6 +90,7 @@ class DCMotor:
         #Find the factor you have to multiply a percentage with to map perfectly from 100% to full_forward_percent
         print(percentage)
         factor = 85/100
-        duty_percentage = self.stop_percentage + factor * percentage
+        duty_percentage = (self.stop_percentage) + factor * percentage
+        print("factor" + str(factor))
         print(duty_percentage)
         return duty_percentage

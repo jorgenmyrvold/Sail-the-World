b3354349ec224bc22c9200fbe40734082f44e8cc
ludvikka <ludkasbo@gmail.com>
2020-10-23 09:19:05 +0200

    New testfunction

--- motor/DCmotor_test.py
+++ motor/DCmotor_test.py
@@ -14,13 +14,37 @@ def test():
     right_backward = 5
     right_pwm = 12
 
+
+
     LeftMotor = DCMotor(left_pwm, left_forward, left_backward, 'Left')
     RightMotor = DCMotor(right_pwm, right_forward, right_backward, 'Right')
 
-    testDirections(LeftMotor)
-    testDirections(RightMotor)
-    testRange(LeftMotor)
-    testRange(RightMotor)
+    #testDirections(LeftMotor)
+    #testDirections(RightMotor)
+    #testRange(LeftMotor)
+    #testRange(RightMotor)
+    testings(LeftMotor,Rightmotor)
+
+
+def testings(motorClass_L, motorClass_R):    
+    motorClass_L.turn_forward(10)
+    motorClass_R.turn_forward(10)
+    sleep(3)
+    motorClass_L.turn_forward(20)
+    motorClass_R.turn_forward(20)
+    sleep(3)
+    motorClass_L.turn_forward(30)
+    motorClass_R.turn_forward(30)
+    sleep(3)
+    motorClass_L.turn_forward(40)
+    motorClass_R.turn_forward(40)
+    sleep(3)
+    motorClass_L.turn_forward(50)
+    motorClass_R.turn_forward(50)
+    sleep(3)
+    motorClass_L.shut_down()
+    motorClass_R.shut_down()
+
 
 def testDirections(motorClass):
     

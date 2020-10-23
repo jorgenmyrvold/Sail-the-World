0abe9a0ed83dcb8392db17e2e5501b046579a7cc
ludvikka <ludkasbo@gmail.com>
2020-10-23 10:25:29 +0200

    test

--- motor/encoder.py
+++ motor/encoder.py
@@ -44,7 +44,7 @@ class Encoder:
     def encoder_callback(self, channel):
         self.current_value = self.current_value + 1
         self.distance_travelled = self.current_value/self.number_of_values_per_round*self.round_distance
-        #print("cuurent_value = ",self.current_value)
+        print("cuurent_value = ",self.current_value)
 
 
     def resetEncoder(self):

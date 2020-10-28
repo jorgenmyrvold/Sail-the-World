1b8388279fed66aebcf318d79595dd720d74d896
jorgenmyrvold <jorgeum@stud.ntnu.no>
2020-10-28 10:11:17 +0100

    implemented raise flag wit servo

--- tasks/raise_flag.py
+++ tasks/raise_flag.py
@@ -10,8 +10,23 @@ def raise_flag():
     GPIO.output(flag_motor_pin, GPIO.HIGH)
     sleep(3)
     GPIO.output(flag_motor_pin,GPIO.LOW)
+
+
+def raise_flag_servo():
+    flag_motor_pin = 7
+    GPIO.setup(flag_motor_pin, GPIO.OUT)
+    servo = GPIO.PWM(flag_motor_pin, 50)   # Pin and 50 = 50Hz
+    servo.start(0)
     
+    servo.ChangeDutyCycle(2)   # Set to 0 pos
+    time.sleep(2)
+    servo.ChangeDutyCycle(12)  # raise flag
+    
+    servo.stop()
+    GPIO.cleanup()
+
+    
+
 if __name__ == "__main__":
     GPIO.setmode(GPIO.BCM)
-    sleep(2)
-    raise_flag()
+    raise_flag_servo()
